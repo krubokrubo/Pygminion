590817ed9b2054910f5cf3be070d233f8afafc46
Paul Dexter <krubokrubo@gmail.com>
2010-12-26 00:18:25 -0500

    Fix money functions to work correctly when over $9

--- trunk/pygminion/utils.py
+++ trunk/pygminion/utils.py
@@ -1,44 +1,32 @@
 import string
 
 def cmpMoney(a, b):
-    try:
-        a = a.lower()
-        b = b.lower()
-        return cmp(int(a), int(b))
-    except ValueError:
-        # Something has a potion in the cost...
+    ''' Whether a can buy b.  cmp('5','3p') == -1 but also cmp('3p','5') == -1. '''
+    a_potions = a.count('p')
+    b_potions = b.count('p')
+    a_coins = int(a.replace('p',''))
+    b_coins = int(b.replace('p',''))
+    if a_coins == b_coins and a_potions == b_potions:
+        return 0
+    elif a_coins < b_coins or a_potions < b_potions:
+        return -1
+    else:
+        return 1
         
-        a_coins = sum([int(c) for c in a if c in string.digits])
-        b_coins = sum([int(c) for c in b if c in string.digits])
-        a_potions = sum([1 for c in a if c == 'p'])
-        b_potions = sum([1 for c in b if c == 'p'])
-        
-        # Equal?
-        if a_coins == b_coins and a_potions == b_potions: return 0
-        
-        elif a_coins < b_coins or a_potions < b_potions: return -1
-        
-        else: return 1
 
 def addMoney(a, b):
-    a = a.lower()
-    b = b.lower()
-    
-    a_coins = sum([int(c) for c in a if c in string.digits])
-    b_coins = sum([int(c) for c in b if c in string.digits])
-    a_potions = sum([1 for c in a if c == 'p'])
-    b_potions = sum([1 for c in b if c == 'p'])
+    a_potions = a.count('p')
+    b_potions = b.count('p')
+    a_coins = int(a.replace('p',''))
+    b_coins = int(b.replace('p',''))
     
     return str(a_coins + b_coins) + "p"*(a_potions + b_potions)
         
 def subtractMoney(a, b):
-    a = a.lower()
-    b = b.lower()
-
-    a_coins = sum([int(c) for c in a if c in string.digits])
-    b_coins = sum([int(c) for c in b if c in string.digits])
-    a_potions = sum([1 for c in a if c == 'p'])
-    b_potions = sum([1 for c in b if c == 'p'])
+    a_potions = a.count('p')
+    b_potions = b.count('p')
+    a_coins = int(a.replace('p',''))
+    b_coins = int(b.replace('p',''))
 
     assert(a_coins >= b_coins and a_potions >= b_potions, "not enough money to subtract :(")
     
